9b57fe2d8d48e029b29f9167468f09b29361f43b
Sergey <dokxus@mail.ru>
2023-01-11 14:36:18 +0900

    Добавил отступы для функций

--- homework_02/base.py
+++ homework_02/base.py
@@ -7,17 +7,20 @@ class Vehicle(ABC):
     default_fuel = 10
     default_fuel_consumption = 25
 
+
     def __init__(self, weight, fuel, fuel_consumption, started=False):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
         self.started = started
 
+
     def start(self):
         if self.fuel > 0:
             self.started = True
         else:
             raise exceptions.LowFuelError
+
 
     def move(self, distance):
         expected_fuel_to_spend = distance * self.fuel_consumption
