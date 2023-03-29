76a548a82d3e83337bb1cc225667e24f83795c68
Sergey <dokxus@mail.ru>
2023-03-29 13:08:04 +0900

    Done

--- homework_07/djapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from djapp.models import User
-
-admin.site.register(User)
--- homework_07/djapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'djapp'
